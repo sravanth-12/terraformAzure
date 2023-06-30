bfdf242d083d2092aac6bdf2f2aaa2b38640c7e7
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-06-30 16:10:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "SR-RG"
+  location = "westus2"
+}
